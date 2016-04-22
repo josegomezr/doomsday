8d17a3b995e1033d200637ed6df883b8fef23734
Valentina Azocar <pizenblues@gmail.com>
2016-04-21 23:34:24 -0430

    1% mas cerca de lograr la vista

--- controller/controller_product.php
+++ controller/controller_product.php
@@ -10,10 +10,12 @@ class product{
 		load_file('view/admin/product.php', $this->view_data);
 	}
 
-	function view($product){
-		$sql_product_view = "SELECT nombre_producto, id_producto, categoria_producto FROM producto 
-		WHERE id_producto = '{$product['id_producto']}'";
-		$this->view_data['product_view'] = Database::connect()->query($sql_product_view)->fetch_all(MYSQLI_ASSOC);
+	function view(){
+		$product_id = $_GET['id_producto'];
+		$sql_product_view = "SELECT nombre_producto, id_producto, categoria_producto FROM producto WHERE id_producto = '$product_id'";
+
+		var_dump($sql_product_view);
+		$this->view_data['product_view'] = Database::connect()->query($sql_product_view)->fetch_assoc();
 		load_file('view/admin/product_view.php', $this->view_data);
 	}
 }	
